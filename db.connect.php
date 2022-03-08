3819318b20dd8203062b85af8788e9fc74a0cc26
celomarshl <marshalmartin002@gmail.com>
2022-03-08 18:39:12 +0000

    march update

---
+++ db.connect.php
@@ -0,0 +1,17 @@
+<?php
+//     $servername ="localhost";
+//     $username = "root";
+//     $password ="";
+//     $dbname = "personal";
+// //connection create
+
+//     $conn = new mysqli ($servername, $username, $password, $dbname);
+//     //connection check
+//         if ($conn->connect_error) {
+//             die ("connectiob failed:" . $conn->connect_error);
+//         }
+//         else {
+//             "";
+//        / }
+
+?>
